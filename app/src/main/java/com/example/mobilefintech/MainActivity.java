6cb2ffe9beabdd5ebf7f9fd6d218e811d2c3ea6b
Daniel Seitenfus <daniel.seitenfus@hotmail.com>
2021-06-26 19:53:28 -0300

    feat: account screen body

--- app/src/main/java/com/example/mobilefintech/MainActivity.java
+++ app/src/main/java/com/example/mobilefintech/MainActivity.java
@@ -13,4 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void click(View view){
+        startActivity(new Intent(this, AccountActivity.class));
+    }
 }
